8744dd3223102e5045f8b19496dd696b08ae0428
Mert Celil <mertcelilozdin@hotmail.com>
2021-03-14 10:25:18 +0300

    email autofill

--- other/whats-a-test/2.todo.js
+++ other/whats-a-test/2.todo.js
@@ -32,7 +32,7 @@ function expect(actual) {
   return {
     toBe(expected){
       if (actual !== expected) {
-        throw new Error(`${result} is not equal to ${expected}`)
+        throw new Error(`${actual} is not equal to ${expected}`)
       }
     }
   }
--- other/whats-a-test/3.todo.js
+++ other/whats-a-test/3.todo.js
@@ -27,22 +27,35 @@ Then run this code with `node 3.todo`
 
 const {sum, subtract} = require('./math')
 
-let result, expected
+test('sum adds numbers', () => {
+  const result = sum(3,7)
+  const expected = 10
+  expect(result).toBe(expected);
+})
 
-result = sum(3, 7)
-expected = 10
-expect(result).toBe(expected)
+test('subtract numbers', () => {
+  const result = subtract(7, 3)
+  const expected = 4
+  expect(result).toBe(expected);
+})
 
-result = subtract(7, 3)
-expected = 4
-expect(result).toBe(expected)
+function test(title, callback) {
+  try {
+    callback()
+    console.log(`OK ${title}`);
+  } catch (error) {
+    console.error(`x ${title}`);
+    console.error(error);
+  }
+}
 
 function expect(actual) {
   return {
-    toBe(expected) {
+    toBe(expected){
       if (actual !== expected) {
         throw new Error(`${actual} is not equal to ${expected}`)
       }
-    },
+    }
   }
 }
+
