e1a65cf892640d3de87505535f674756b6d305f0
Mert Celil <mertcelilozdin@hotmail.com>
2021-03-17 09:45:15 +0300

    server testing with mock and fake data

--- server/src/controllers/__tests__/users.todo.js
+++ server/src/controllers/__tests__/users.todo.js
@@ -1,14 +1,14 @@
-//////// Elaboration & Feedback /////////
-// When you've finished with the exercises:
-// 1. Copy the URL below into your browser and fill out the form
-// 2. remove the `.skip` from the test below
-// 3. Change submitted from `false` to `true`
-// 4. And you're all done!
-/*
-http://ws.kcd.im/?ws=Testing&e=users%20test$20object%20factories&em=m720694@outlook.com
-*/
-test.skip('I submitted my elaboration and feedback', () => {
-  const submitted = false // change this when you've submitted!
-  expect(submitted).toBe(true)
+import * as usersController from '../users'
+import {initDb} from 'til-server-test-utils'
+
+test('getUsers returns all users in the database', async()=>{
+  await initDb()
+  const req = {}
+  const res = {
+    json: jest.fn()
+  }   
+  await usersController.getUsers(req,res)
+  expect(res.json).toHaveBeenCalledTimes(1)
+  // expect(res.json).toHaveBeenCalled() sayı önemsiz ve sadece çağrılıp çağrılmadığı için
+  console.log(res.json.mock.calls[0]);
 })
-////////////////////////////////
