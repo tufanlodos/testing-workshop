dbf2e3c7c153f6004ed2e719e17db3665b547415
Mert Celil <mertcelilozdin@hotmail.com>
2021-03-17 08:53:34 +0300

    thumb war 1 and 2

--- other/whats-a-mock/__tests__/thumb-war.2.todo.js
+++ other/whats-a-mock/__tests__/thumb-war.2.todo.js
@@ -7,10 +7,22 @@ test('returns winner', () => {
   // change the getWinner implementation to a function
   // that keeps track of how often it's called and
   // the arguments it's called with (Hint #1)
-  utils.getWinner = (p1, p2) => p2
+
+  utils.getWinner = (...args) => {
+    utils.getWinner.mock.calls.push(args) //her bir çağrıda parametreleri (p1 ve p2) ekliyoruz
+    return args[1] // 1. indexte olan yani 2. parametreyi, p2 yi dönüyoruz
+  }
+
+  utils.getWinner.mock = {calls:[]} //yukarıda getWinner ı tanımladık ve assertionlar başlamadan önce boş array ile init ettik. malum jsde function lar first class object olduğundan propertye de sahip olabiliyor
 
   const winner = thumbWar('Ken Wheeler', 'Kent C. Dodds')
   expect(winner).toBe('Kent C. Dodds')
+  // console.log(utils.getWinner.mock.calls); // 2 defa çağrılır çünkü thumbWar içinde bir kullanıcının 2 olması oyunu kazandırıyor [ [ 'Ken Wheeler', 'Kent C. Dodds' ], [ 'Ken Wheeler', 'Kent C. Dodds' ] ]
+  expect(utils.getWinner.mock.calls).toHaveLength(2) // 2 defa çağrılır çünkü thumbWar içinde bir kullanıcının 2 olması oyunu  kazandırıyor. Bunun yerine direk toEqual ile üstte konsoldan gelen çıktıyı direk da yazabilirdik
+  utils.getWinner.mock.calls.forEach(args => {
+    expect(args).toEqual(['Ken Wheeler', 'Kent C. Dodds'])
+  }) // her seferinde 2 elemanlı arrayimiz ile mi çağrılmış kontrolü
+
   // add an assertion for how many times the getWinner function
   // was supposed to be called (2 times) (Hint #2)
   //
